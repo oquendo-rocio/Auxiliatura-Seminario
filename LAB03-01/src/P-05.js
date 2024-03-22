93a1ae67fd3986f55fe93f090445fab4ec4d01aa
Rocio Oquendo <oquendoramirezrocio@gmail.com>
2024-03-22 18:00:59 -0400

    refresh

--- LAB03-01/src/P-05.js
+++ LAB03-01/src/P-05.js
@@ -1,13 +1,5 @@
-function sortFruits(fruits) {
-	return fruits.sort();
-} 
+const sortFruits = fruits => fruits.sort();
 
-function getElementInTheMiddle(list) {
-	return list[list.length / 2];
-}
+const getElementInTheMiddle = list => list[Math.floor(list.length / 2)];
 
-function adminLogin(user) {
-	if(user == "ADMIN") return true;
-	if(user == "DEV") return true;
-	return false;
-}
+const adminLogin = user => user === "ADMIN" || user === "DEV";
